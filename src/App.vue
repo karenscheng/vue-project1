91dddc420d7c812d31a6516ffea0e08e905a1c3c
kscheng <kscheng@dhcp-v880-17c-00382.1Xwireless.unc.edu>
2017-02-15 11:44:32 -0500

    adding all initial files

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div id="panel">
+      <Hello></Hello>
+    </div>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: rgb(255, 255, 255);
+  background-image: url("../images/cluster.jpg");
+  display: flex;
+  /*border: 5px solid red;*/
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
+}
+
+#panel{
+  background: rgba(255, 255, 255, 0.6);
+  height: 80vh;
+  width: 70vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  /*border: 5px solid red;*/
+}
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <h1>WHICH GALAXY ARE YOU?</h1>
+    <button type="button"><span>TAKE THE QUIZ</span></button>
+  </div>
+</template>
+
+<style scoped>
+
+  .hello{
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1{
+    /*border: 5px solid red;*/
+    background: url("../../images/cluster.jpg") no-repeat;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 18vmin;
+    text-align: center;
+    font-weight: bold;
+  }
+
+  button{
+    display: flex;
+    background-color: rgba(15, 82, 186, .6);
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    align-content: center;
+    font-size: 4vmin;
+    cursor: pointer;
+    margin-top: -50px;
+  }
+
+  /*button span{
+    color: rgba(255, 255, 255, 0.6);
+  }*/
+
+  @media only screen and (max-width: 1250px) {
+
+    h1{
+      font-size: 14vmin;
+    }
+  }
+
+</style>
